f6b356009cf1bc02d79c403401c1b7568cee78fb
“[metidev]” <[anwrymhdy1@gmail.com]>
2023-08-24 23:25:54 +0330

    add new style

--- index.php
+++ index.php
@@ -25,72 +25,80 @@ function stringToColor($string)
 <html>
 
 <head>
-  <title>METI local</title>
-  <link rel="shortcut icon" type="image/png" href="favicon.png">
-  <link href="bootstrap-5.3.1/css/bootstrap.min.css" rel="stylesheet">
-  <link href="bootstrap-5.3.1/custom.css" rel="stylesheet">
-  <script src="bootstrap-5.3.1/js/bootstrap.bundle.min.js"></script>
-  <script src="bootstrap-5.3.1/js.js" type="text/javascript"></script>
-  <link rel="icon" href="bootstrap-5.3.1/favicon-1.png" type="image/png">
-<style>
-  *{
-    font-family: sans-serif;
-  }
-</style>
+    <title>METI local</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <link href="bootstrap-5.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap-5.3.1/custom.css" rel="stylesheet">
+    <script src="bootstrap-5.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap-5.3.1/js.js" type="text/javascript"></script>
+    <link rel="icon" href="bootstrap-5.3.1/favicon-1.png" type="image/png">
+    <style>
+    * {
+        font-family: sans-serif;
+    }
+    </style>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg fixed-top">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="bootstrap-5.3.1/favicon.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-        <strong>MΞΓI</strong>
-      </a>
-
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="http://localhost">Home</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Tools
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://github.com/metidev" target="_blank">
+                <img src="bootstrap-5.3.1/favicon.png" alt="Logo" width="30" height="24"
+                    class="d-inline-block align-text-top">
+                <strong>MΞΓI</strong>
             </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="http://localhost/phpmyadmin/">PhpMyAdmin</a></li>
-              <li><a class="dropdown-item" href="http://localhost/?q=info">PHP Info</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="http://localhost/redis/?overview">Redis WA</a></li>
-              <li><a class="dropdown-item" href="http://localhost/memcached/">Memecached WA</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">PHP:
-              <?php print phpversion(); ?>
-            </a>
-          </li>
 
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2 search" tabindex="1" type="text" id="search" placeholder="Type To search..."
-            aria-label="Search">
-        </form>
-      </div>
-    </div>
-  </nav>
-  <div class="container">
-    <div class="content">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="http://localhost">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Tools
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="http://localhost/phpmyadmin/">PhpMyAdmin</a></li>
+                            <li><a class="dropdown-item" href="http://localhost/?q=info">PHP Info</a></li>
+                          
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">PHP:
+                            <?php print phpversion(); ?>
+                        </a>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <!-- <input class="form-control me-2 search" tabindex="1" type="text" id="search"
+                        placeholder="Type To search..." aria-label="Search"> -->
+
+                    <div class="flexbox">
+                        <div class="search">
+                            <div>
+                                <input tabindex="1" type="text" id="search" type="text" placeholder="Search . . ." required>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            </form>
+        </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="content">
 
 
-      <div class="Directories row">
+            <div class="Directories row">
 
-        <?php
+                <?php
         $Directories = glob(dirname(__FILE__) . '/*', GLOB_ONLYDIR);
         $temp = [];
         foreach ($Directories as $key => $value) {
@@ -102,22 +110,22 @@ function stringToColor($string)
             continue;
           }
           $FirstChar = strtoupper(substr($Dir, 0, 1));
-          echo '<div class="col-md-4"><a href="http://' . $DomainPostFix . '/' . basename($Dir) . '"><span style="background-color:' . stringToColor(strtolower($FirstChar)) . '">' . $FirstChar . '</span>' . basename($Dir) . '</a></div>';
+          echo '<div class="col-md-4"><a href="http://' . $DomainPostFix . '/' . basename($Dir) . '"><span style="background: linear-gradient(90deg, ' . stringToColor(strtolower($FirstChar)). '7d' . ' 0%, ' . stringToColor(strtolower($FirstChar)) . ' 50%, rgba(0, 0, 0, 0) 99%);">' . $FirstChar . '</span>' . basename($Dir) . '</a></div>';
         }
         ?>
-      </div>
-      <div class="info"><br />
-        <?php print($_SERVER['SERVER_SOFTWARE']); ?><br />
-        PHP version:
-        <?php print phpversion(); ?> <span><a title="phpinfo()" href="/?q=info">info</a></span><br />
-        Document Root:
-        <?php print($_SERVER['DOCUMENT_ROOT']); ?><br />
+            </div>
+            <div class="info"><br />
+                <?php print($_SERVER['SERVER_SOFTWARE']); ?><br />
+                PHP version:
+                <?php print phpversion(); ?> <span><a title="phpinfo()" href="/?q=info">info</a></span><br />
+                Document Root:
+                <?php print($_SERVER['DOCUMENT_ROOT']); ?><br />
 
-      </div>
+            </div>
+
+        </div>
 
     </div>
-
-  </div>
 </body>
 
 </html>
